3fb16237b3b7bb4dc0011d77540d51d03ac532e5
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-01-29 18:45:57 -0800

    wip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const translate = require('./translate');
+const translate = require("./translate");
 
 function translateEnglishString(accessKey, key, value, frenchOverrides, germanOverrides) {
   return new Promise((resolve, reject) => {
@@ -11,45 +11,66 @@ function translateEnglishString(accessKey, key, value, frenchOverrides, germanOv
         }
         // Collate results
         const translations = {
+          key: key,
           fr: frenchOverrides.hasOwnProperty(key) ? frenchOverrides[key] : json[0].translations.find(element => element.to == "fr").text,
           de: germanOverrides.hasOwnProperty(key) ? germanOverrides[key] : json[0].translations.find(element => element.to == "de").text,
           en: value
         };
-        const translation = new Object();
-        translation[key] = translations;
-        resolve(translation);
+        resolve(translations);
       })
       .catch(reject);
   })
 }
 
 function generateZosTranslationFileContent(translations) {
-  const fileContent = { en: {}, fr: {}, de: {} };
-  for (let key in translations) {
-    fileContent.en[key] = `ZO_CreateStringId("${key}", "${translations[key].en}")\n`;
-    fileContent.fr[key] = `ZO_CreateStringId("${key}", "${translations[key].fr}")\n`;
-    fileContent.de[key] = `ZO_CreateStringId("${key}", "${translations[key].de}")\n`;
-  }
+  const fileContent = { en: "", fr: "", de: "" };
+  translations.forEach(translation => {
+    fileContent.en += `ZO_CreateStringId("${translation.key}", "${translation.en}")\n`;
+    fileContent.fr += `ZO_CreateStringId("${translation.key}", "${translation.fr}")\n`;
+    fileContent.de += `ZO_CreateStringId("${translation.key}", "${translation.de}")\n`;
+  });
 
   return fileContent;
 }
 
-function writeZosLocalizationFile(lang, fileContent, directory) {
-  const fse = require('fs-extra');
-  const path = `${directory}/${lang}.lua`;
-  return fse.ensureFile(path)
-    .then(() => fse.writeFile(path, fileContent));
-}
-
 // Translates from English to French and German.
 function translateEnglishStrings(accessKey, en, fr, de, destinationDirectory) {
+  const fs = require("fs");
+  const path = require("path");
   return Promise.all(Object.keys(en).map(key => translateEnglishString(accessKey, key, en[key], fr, de)))
     .then(generateZosTranslationFileContent)
-    .then(fileContent => Object.keys(fileContent).map(lang => writeZosLocalizationFile(lang, fileContent[lang], destinationDirectory)))
-    .then(promises => Promise.all(promises));
+    .then(fileContent => Object.keys(fileContent).map(lang => {
+      const languageFile = `${destinationDirectory}/${lang}.lua`;
+      return new Promise((resolve, reject) => {
+        fs.exists(path.dirname(languageFile), exists => {
+          if (!exists) {
+            fs.mkdir(path.dirname(languageFile), error => {
+              if (error) {
+                reject(error);
+              }
+
+              resolve();
+            })
+          }
+
+          resolve();
+        });
+      })
+        .then(() => new Promise((resolve, reject) => {
+          fs.writeFile(languageFile, fileContent[lang], { flag: 'w+' }, error => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve();
+            }
+          });
+        }));
+    }))
+    .then(promises => {
+      return Promise.all(promises)
+    });
 }
 
 module.exports.translateEnglishStrings = translateEnglishStrings;
 module.exports.translateEnglishString = translateEnglishString;
 module.exports.generateZosTranslationFileContent = generateZosTranslationFileContent;
-module.exports.writeZosLocalizationFile = writeZosLocalizationFile;
--- test/translator.test.js
+++ test/translator.test.js
@@ -1,44 +1,45 @@
 const rimraf = require("rimraf");
-const esoTranslate = require('../index');
+const esoTranslate = require("../index");
+
+const destinationDirectory = "./test/translated";
 
 function cleanTranslatedStrings() {
-	return new Promise(resolve => rimraf('./strings/translated', resolve));
+	return new Promise(resolve => rimraf(destinationDirectory, resolve));
 }
 
 function basicAssertTranslateAssert(translation) {
 	expect(translation).not.toBeUndefined();
-	expect(Object.keys(translation).length).toBe(1);
-	expect(translation["key1"]).not.toBeUndefined();
-	expect(translation["key1"].fr).not.toBeUndefined();
-	expect(translation["key1"].de).not.toBeUndefined();
-	expect(translation["key1"].en).not.toBeUndefined();
-	expect(translation["key1"].en).toBe("value 1");
+	expect(translation.key).not.toBeUndefined();
+	expect(translation.fr).not.toBeUndefined();
+	expect(translation.de).not.toBeUndefined();
+	expect(translation.en).not.toBeUndefined();
 	return translation;
 }
 
-beforeEach(cleanTranslatedStrings);
+// beforeEach(cleanTranslatedStrings);
 
-afterAll(cleanTranslatedStrings);
+// afterAll(cleanTranslatedStrings);
 
-test('Strings are translated from English', () => {
-	return esoTranslate.translateEnglishString(process.env['AzureTranslatorKey'], "key1", "value 1", {}, {})
-		.then(basicAssertTranslateAssert);
+describe("Translate string", () => {
+	test("Strings are translated from English", () => {
+		return esoTranslate.translateEnglishString(process.env["AzureTranslatorKey"], "key1", "value 1", {}, {})
+			.then(basicAssertTranslateAssert);
+	});
+
+	test("Strings are overridden when provided", () => {
+		return esoTranslate.translateEnglishString(process.env["AzureTranslatorKey"], "key1", "value 1", { "key1": "Custom French Value" }, { "key1": "Custom German Value" })
+			.then(basicAssertTranslateAssert)
+			.then(translation => {
+				expect(translation.key).toBe("key1");
+				expect(translation.fr).toBe("Custom French Value");
+				expect(translation.de).toBe("Custom German Value");
+				expect(translation.en).toBe("value 1");
+			});
+	});
 });
 
-test('Strings are overridden when provided', () => {
-	return esoTranslate.translateEnglishString(process.env['AzureTranslatorKey'], "key1", "value 1", { "key1": "Custom French Value" }, { "key1": "Custom German Value" })
-		.then(basicAssertTranslateAssert)
-		.then(translation => {
-			expect(translation["key1"].fr).toBe("Custom French Value");
-			expect(translation["key1"].de).toBe("Custom German Value");
-		});
-});
+test("Translation fails if invalid access key provided", () => expect(esoTranslate.translateEnglishString("foo", "key1", "value 1", {}, {})).rejects.toThrowError());
 
-test('Translation fails if invalid access key provided', () => {
-	return expect(esoTranslate.translateEnglishString('foo', "key1", "value 1", {}, {})).rejects.toThrowError();
-});
-
-test('E2E', () => {
-	return esoTranslate.translateEnglishStrings(process.env["AzureTranslatorKey"], { "key1": "Value 1", "key2": "Value 2" }, {}, {}, './strings/translations')
-		.then(console.log);
+test("E2E", () => {
+	return esoTranslate.translateEnglishStrings(process.env["AzureTranslatorKey"], { "key1": "Value 1", "key2": "Value 2" }, {}, {}, destinationDirectory);
 });
