b100d4fc15553cbe44568d02de4957418ee015e0
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-02-01 22:10:31 -0800

    wip

--- build.js
+++ build.js
@@ -1,42 +1,39 @@
-module.exports = (buildDirectory) => {
-  const fs = require('fs');
-  const path = require('path');
-  const rimraf = require('rimraf');
-  const buildConfig = require('./');
-  const resolvedPath = path.resolve(buildDirectory);
-  return new Promise((resolve, reject) => {
-    fs.exists(resolvedPath, exists => {
-      if (exists) {
-        rimraf(resolvedPath, error => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve();
-          }
-        })
-      } else {
-        resolve();
-      }
-    });
-  })
-    .then(() => new Promise((resolve, reject) => {
-      fs.mkdir(resolvedPath, error => {
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const buildConfig = require('./build.json');
+const copy = require('copy');
+const resolvedPath = path.resolve(buildConfig.buildDirectory);
+return new Promise((resolve, reject) => {
+  fs.exists(resolvedPath, exists => {
+    if (exists) {
+      rimraf(resolvedPath, error => {
         if (error) {
           reject(error);
         } else {
           resolve();
         }
       })
-    }))
-    .then(() => new Promise((resolve, reject) => {
-      fs.copyFile('./index.js', `${resolvedPath}/index.js`, error => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
-    }));
-}
-
-module.exports('./build');
+    } else {
+      resolve();
+    }
+  });
+})
+  .then(() => new Promise((resolve, reject) => {
+    fs.mkdir(resolvedPath, error => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    })
+  }))
+  .then(() => buildConfig.buildFiles.map(glob => new Promise((resolve, reject) => {
+    copy(glob, resolvedPath, error => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  })));
